c44fec90fab2cdeeb108ce7c1758e8ba292a85a5
Victor <araujo.victor164@gmail.com>
2024-09-25 14:36:51 -0300

    Blogchanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './shared/components/Header/header.component';
 import { FooterComponent } from './shared/components/Footer/footer.component';
+import { BlogComponent } from './features/blog/blog.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent, BlogComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
