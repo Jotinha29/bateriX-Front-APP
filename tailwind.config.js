090ace8154a9bc24cf1d3a167d02ae6338b85ae0
joao.calza <joao.calza@memora.com.br>
2024-11-05 20:24:32 -0300

    Home 90%

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lora: ['Lora', 'sans-serif'],
+        'lora-bold': ['Lora-Bold', 'sans-serif'],
+        'lora-semi-bold': ['Lora-SemiBold', 'sans-serif'],
+        montserrat: ['montserrat', 'sans-serif'],
+        'montserrat-bold': ['Montserrat-Bold', 'sans-serif'],
+        'montserrat-semi-bold': ['Montserrat-SemiBold', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 };
